28bb99b73e6eaaa1add13d625c1fe472e399269a
Aysan Isayo <aisayo@icloud.com>
2018-09-29 15:54:41 -0700

    imported Form elements from RNE for Login form

--- components/screens/Login.js
+++ components/screens/Login.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
+import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements'
+
 
 class Login extends Component {
   render() {
     return (
       <View>
-        <Text> Login Component </Text>
+          <View>
+            <FormLabel>Username:</FormLabel>
+            <FormInput onChangeText={console.log('typing')}/>    
+            <FormLabel>Password:</FormLabel>
+            <FormInput />
+          </View>
       </View>
     )
   }
