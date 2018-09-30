88128031593c0163b491459e0bbedcdb608287cf
Aysan Isayo <aisayo@icloud.com>
2018-09-30 15:47:20 -0700

    added style to settings and quoteindex components

--- app/components/QuotesIndex.js
+++ app/components/QuotesIndex.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 
 export default class QuotesIndex extends Component {
   render() {
     return (
-      <View>
+      <View style={styles.text}>
         <Text> Quotes index </Text>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  text: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+  }
+}
+)
--- app/components/Settings.js
+++ app/components/Settings.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 
 export default class Settings extends Component {
   render() {
     return (
-      <View>
+      <View style={styles.text}>
         <Text> Settings </Text>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+    text: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+}
+)
