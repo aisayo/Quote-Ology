30a57761e35041480bba32f59d69ce50f8d68773
Aysan Isayo <aisayo@icloud.com>
2018-10-07 19:08:06 -0700

    refractored welcome message into its own component and accepts props for name

---
+++ app/components/common/Welcome.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+
+
+export const Welcome = (props) => {
+    return(
+        <Text style={styles.textStyle}>Welcome, {props.name}!</Text>
+        )
+}
+
+const styles = StyleSheet.create({
+    textStyle: {
+        alignSelf: 'center',
+        paddingLeft: 25,
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'gray'
+    }
+})
+
--- app/components/screens/Home.js
+++ app/components/screens/Home.js
@@ -2,32 +2,24 @@ import React, { Component } from 'react'
 import { View, StyleSheet, Text } from 'react-native'
 
 import { Header } from '../common/Header';
-import { ProfilePic } from '../common/ProfilePic'
+import { ProfilePic } from '../common/ProfilePic';
+import { Welcome } from '../common/Welcome';
 
 export const Home = () => {
 
     return (
-        <View style={styles.homeStyle}>
+        <View >
             <Header text={'Home'}/>
             <View style={styles.profileStyle}>
                 <ProfilePic src={require('../../../assets/img/profileimg1.jpg')}/>
-                <Text style={styles.textStyle}>Welcome, Aysan Isayo!</Text>
+                <Welcome name={'Aysan Isayo'}/>
             </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    homeStyle: {
-    },
     profileStyle: {
         flexDirection: 'row',
-    },
-    textStyle: {
-        alignSelf: 'center',
-        paddingLeft: 25,
-        fontSize: 20,
-        fontWeight: 'bold',
-        color: 'gray'
     }
 })
